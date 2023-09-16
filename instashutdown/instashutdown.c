0fa23b5cd7ac36dd7ff1a64ad90eeaccb934bb03
notcha3 <notcha3>
2023-09-16 18:30:02 +0300

    Idk, uploading.

---
+++ instashutdown/instashutdown.c
@@ -0,0 +1,39 @@
+﻿#include "instashutdown.h"
+
+VOID main(VOID)
+{
+	
+	HMODULE hNTDLL = GetModuleHandleA("ntdll.dll");
+
+	PVOID pVoid = NULL;
+
+	RtlAdjustPrivilege = (NTSTATUS*)GetProcAddress(
+		hNTDLL,
+		"RtlAdjustPrivilege"
+	);
+
+	NtShutdownSystem = (NTSTATUS*)GetProcAddress(
+		hNTDLL,
+		"NtShutdownSystem"
+	);
+
+
+	NTSTATUS uPrivStatus = RtlAdjustPrivilege(
+		SE_SHUTDOWN_PRIVILEGE, 
+		TRUE, 
+		FALSE, 
+		&pVoid);
+
+	if(uPrivStatus == STATUS_SUCCESS) {
+		NtShutdownSystem(ShutdownPowerOff);
+		FreeLibrary(hNTDLL);
+	}
+	else {
+		FreeLibrary(hNTDLL);
+		MessageBoxA(NULL,
+		"Couldn't get shutdown privilege\0",
+		"Error\0",
+		MB_OK | MB_ICONERROR);
+	}
+
+}
---
+++ instashutdown/instashutdown.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+
+#define WIN32_LEAN_AND_MEAN
+
+#include <windows.h>
+#include <winternl.h>
+#include <ntstatus.h>
+
+// thanks to reactos sources and https://undocumented.ntinternals.click for the function prototypes
+
+#define SE_SHUTDOWN_PRIVILEGE 19
+
+NTSTATUS (NTAPI *RtlAdjustPrivilege) (
+    _In_ INT    Privilege,
+	_In_ BOOLEAN  Enable,
+	_In_ BOOLEAN  CurrentThread,
+	_In_ PBOOLEAN Enabled
+);
+
+typedef enum SHUTDOWN_ACTION {
+    ShutdownNoReboot,
+    ShutdownReboot,
+    ShutdownPowerOff
+} SHUTDOWN_ACTION, *PSHUTDOWN_ACTION;
+
+NTSTATUS (NTAPI *NtShutdownSystem) (
+  _In_ SHUTDOWN_ACTION Action 
+  );
